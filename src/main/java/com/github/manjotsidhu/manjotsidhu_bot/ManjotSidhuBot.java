1e174177fe722dc01b5aeb3f374e3bfd8f94f571
Manjot Sidhu <manjot.techie@gmail.com>
2018-11-05 18:21:10 +0530

    Updates

--- src/main/java/com/github/manjotsidhu/manjotsidhu_bot/ManjotSidhuBot.java
+++ src/main/java/com/github/manjotsidhu/manjotsidhu_bot/ManjotSidhuBot.java
@@ -1,13 +1,16 @@
 package com.github.manjotsidhu.manjotsidhu_bot;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class ManjotSidhuBot extends TelegramLongPollingBot {
+
     String botToken = "";
-            
+
     ManjotSidhuBot(String token) {
         botToken = token;
     }
@@ -16,13 +19,36 @@ public class ManjotSidhuBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText("Message Recieved:\n" + update.getMessage().getText());
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            
+            String input = update.getMessage().getText();
+            Integer inputId = update.getMessage().getMessageId();
+            long chatId = update.getMessage().getChatId();
+
+            Pattern ptn = Pattern.compile("\\/(\\w+)\\s(\\w+)\\s(.+)");
+            Matcher mtch = ptn.matcher(input);
+
+            if (mtch.find()) {
+                SendMessage message = new SendMessage()
+                        .setChatId(chatId)
+                        .setReplyToMessageId(inputId)
+                        .setText("Test Passed");
+
+                try {
+                    execute(message); // Call method to send the message
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                SendMessage message = new SendMessage()
+                        .setChatId(chatId)
+                        .setReplyToMessageId(inputId)
+                        .setText(Templates.ERROR);
+
+                try {
+                    execute(message); // Call method to send the message
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -34,7 +60,7 @@ public class ManjotSidhuBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        if(botToken.equals("")) { 
+        if (botToken.equals("")) {
             System.err.println("Bot API token not set");
             System.exit(1);
         }
---
+++ src/main/java/com/github/manjotsidhu/manjotsidhu_bot/Templates.java
@@ -0,0 +1,7 @@
+package com.github.manjotsidhu.manjotsidhu_bot;
+
+public class Templates {
+
+    public static final String ERROR = "Sorry, I didn't understood what you just said";
+
+}
